912b22ec436167ee11cb45d1a4b7f7462d4fc2a4
Alex S <hlcfan.yan@gmail.com>
2026-05-09 14:37:05 +0800

    Only remove the padding for the response body pane

--- src/ui.rs
+++ src/ui.rs
@@ -4556,14 +4556,24 @@ impl BeamView {
     }
 
     fn render_response_panel(&self, _: &mut Window, cx: &mut Context<Self>) -> Div {
-        let response_container = div()
-            .flex_1()
-            .w_full()
-            .rounded(px(8.0))
-            .border_1()
-            .border_color(rgb(0xd1d5db))
-            .p_0()
-            .child(self.render_response_editor_surface(cx));
+        let response_container = match self.active_response_tab {
+            ResponseTab::Body => div()
+                .flex_1()
+                .w_full()
+                .rounded(px(8.0))
+                .border_1()
+                .border_color(rgb(0xd1d5db))
+                .p_0()
+                .child(self.render_response_editor_surface(cx)),
+            ResponseTab::Headers => div()
+                .flex_1()
+                .w_full()
+                .rounded(px(8.0))
+                .border_1()
+                .border_color(rgb(0xd1d5db))
+                .p_3()
+                .child(self.render_response_editor_surface(cx)),
+        };
 
         v_flex()
             .h_full()
