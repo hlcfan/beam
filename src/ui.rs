b27cd224ca7ac3e0ca4348c22d9217fdbaa733ba
Alex S <hlcfan.yan@gmail.com>
2026-06-06 17:11:33 +0800

    Update deps

--- src/ui.rs
+++ src/ui.rs
@@ -6878,7 +6878,7 @@ impl BeamView {
                                 elements
                             }
                         })
-                        .flex_grow()
+                        .flex_grow_1()
                         .size_full()
                         .with_sizing_behavior(ListSizingBehavior::Auto)
                         .track_scroll(&self.collection_scroll_handle),
